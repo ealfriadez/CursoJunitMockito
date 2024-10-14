fdbf9d04ddb2602bf07236592a32ad857b4b0ec8
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-10-14 15:06:15 -0500

    First commit

---
+++ src/main/java/pe/edu/unfv/courses/CursoJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJunitMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/unfv/courses/CursoJunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJunitMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
